e443a849248de6fe1ddccf5e77058898ce066236
JSR7 <G!thUb1!>
2017-06-08 16:00:10 -0500

    update Logout Servlet on  Cookies Ex

---
+++ src/main/java/logoutServlet.java
@@ -0,0 +1,22 @@
+/**
+ * Created by jsr on 6/8/17.
+ */
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+
+
+@WebServlet(name="logoutServlet", urlPatterns = "/logout")
+
+public class logoutServlet extends HttpServlet {
+    protected void  doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.getSession().removeAttribute("user");
+        request.getSession().invalidate();
+        response.sendRedirect("/login");
+    }
+}
